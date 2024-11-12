02217ab5f91bbdc5577520c675f0a1cacd20e591
Asu1a <abdukarimov.05@gmail.com>
2024-11-12 23:03:32 +0500

    new-king-object

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,14 @@ public class Main {
         Characters knight = CharacterFactory.createCharacter("knight");
         knight.setWeaponBehavior(new SwordBehavior());
 
+        Characters knight2 = CharacterFactory.createCharacter("knight");
+        knight.setWeaponBehavior(new AxeBehavior());
+
         game.addSubscriber(king);
         game.removeSubscriber(king);
 
         game.addSubscriber(knight);
+        game.addSubscriber(knight2);
 
 
         game.newUpdate("Added two bosses, and 3 new types of weapons");
